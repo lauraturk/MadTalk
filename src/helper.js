3c415b4d8d1db3e046867a921246d2a075c4d6a7
Anders Wood <anderswood@gmail.com>
2017-07-24 14:38:57 -0600

    finish distribution helper func

--- src/helper.js
+++ src/helper.js
@@ -8,12 +8,35 @@ export default class Helper {
   }
 
   async distributeSelections () {
-    let text = await this.getTextSample()
-    console.log(text);
+    const text = await this.getTextSample()
+    let wordTallies = text.reduce((acc, wordObj) => {
+      !acc.all[wordObj.type]
+        ? acc.all[wordObj.type] = 1
+        : acc.all[wordObj.type] ++;
 
-    //Anders
-    //use text sample
-    // return qtyNounSelected, qtyVerbSelected, qtyAdjSelected
+      let coreType = wordObj.type.substring(0,2)
+      !acc.core[coreType]
+        ? acc.core[coreType] = 1
+        : acc.core[coreType] ++;
+
+      return acc;
+    }, {all: {}, core: {}} )
+    const pctSelections = 9; // Adjust to increase qty of selections
+    const qtyWords = text.length;
+    const qtyCoreWords = Object.keys(wordTallies.core).reduce((acc, wordType) => {
+      if (wordType === 'NN' || wordType === 'VB' || wordType === 'JJ') {
+        acc += wordTallies.core[wordType]
+      }
+      return acc
+    }, 0);
+    const qtySelectedWords = Math.round(pctSelections / 100 * qtyWords);
+    const wordTalliesByCore = {
+      qtyNounSelected: Math.round(wordTallies.core['NN'] / qtyCoreWords * qtySelectedWords),
+      qtyVerbSelected: Math.round(wordTallies.core['VB'] / qtyCoreWords * qtySelectedWords),
+      qtyAdjSelected: Math.round(wordTallies.core['JJ'] / qtyCoreWords * qtySelectedWords),
+    }
+console.log(wordTalliesByCore);
+    return wordTalliesByCore;
   }
 
   async randomizeSelections (qtyNounSelected, qtyVerbSelected, qtyAdjSelected) {
