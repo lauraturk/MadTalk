5ca61a195bf373dc472c71e85b1ff91ec2dc6e37
Chris Bandrowsky <chris.bandrowsky@gmail.com>
2017-07-25 15:57:00 -0600

    more minor text fixes

--- src/helper.js
+++ src/helper.js
@@ -6,7 +6,6 @@ export default class Helper {
     return textSample;
   }
 
-
   async distributeSelections (text) {
     // const text = await this.getTextSample()
     let wordTallies = text.reduce((acc, wordObj) => {
