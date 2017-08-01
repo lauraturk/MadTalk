43b85b9428fadadc4adec819c77f9dd31180d435
Laura Turk <laura.lk.turk@gmail.com>
2017-08-01 16:49:02 -0600

    first game input test

--- src/tests/GameInput.test.js
+++ src/tests/GameInput.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import GameInput from '../components/GameInput';
+import { GameInput } from '../components/GameInput';
 import { shallow, mount } from 'enzyme';
 import fetchMock from 'fetch-mock';
 import { resolveAfter2Seconds, mockFetchCalls } from './testHelper'
@@ -9,3 +9,11 @@ import wordStub from './wordStub.js'
 import textStub from './textStub.js'
 
 import { Route, Link, NavLink, BrowserRouter as Router } from 'react-router-dom'
+
+describe('Game Input tests', () => {
+  it('1. Renders without crashing', () => {
+    const wrapper = shallow(<GameInput wordInfo={{word: 'ball', type: 'NN', index: 1}}/>);
+
+    expect(wrapper.find('h4').text()).toEqual('singular noun')
+  });
+});
