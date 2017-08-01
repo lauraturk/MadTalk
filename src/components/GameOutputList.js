b465e3f56649aef287a86720159bd590338f6eb3
Anders Wood <anderswood@gmail.com>
2017-08-01 10:51:51 -0600

    fix speechsnyth readout

--- src/components/GameOutputList.js
+++ src/components/GameOutputList.js
@@ -5,7 +5,6 @@ import { SpeechSynth } from './SpeechSynth';
 /* eslint-enable */
 
 export const GameOutputList = ({ textSample, gameInputWords, speechEnabled }) => {
-  console.log(gameInputWords);
   if (!textSample.textSample) { return <div></div>; }
   let indexedTextSample = textSample.textSample[0].body.split(' ');
   let punctuationArr = ["'", ',', '.', '?', '!', '-', '$', '&', '(', ')', 's', 't', 'nt', 'd', 've'];
@@ -27,14 +26,18 @@ export const GameOutputList = ({ textSample, gameInputWords, speechEnabled }) =>
       outputWordSection={ word }/>;
   });
 
-  const outputSynthEnd = () => {
-    console.log('Put something here to tell user the reading is done');
-  };
+  let gameReadout = populatedOutput.reduce((acc, text) => {
+    let phrase = text.props.outputWordSection;
+    phrase.charAt(0) === '#'
+      ? acc += phrase.split('#')[1].split('%')[0]
+      : acc += phrase;
+    return acc;
+  }, '');
 
   return (
     <section className='GameOutput-container'>
       <div className='GameOutput-textContainer'>
-        {!speechEnabled ? null : <SpeechSynth text={ joinedWords } onSynthEnd={outputSynthEnd.bind(this)} />}
+        {!speechEnabled ? null : <SpeechSynth text={ gameReadout } onSynthEnd={() => null} />}
         { populatedOutput }
       </div>
     </section>
