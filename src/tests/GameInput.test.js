0de7316691c42b695ba1757c93b6a7d1018ae922
Laura Turk <laura.lk.turk@gmail.com>
2017-08-01 20:34:46 -0600

    game input tests

--- src/tests/GameInput.test.js
+++ src/tests/GameInput.test.js
@@ -11,9 +11,31 @@ import textStub from './textStub.js'
 import { Route, Link, NavLink, BrowserRouter as Router } from 'react-router-dom'
 
 describe('Game Input tests', () => {
-  it('1. Renders without crashing', () => {
+  it('1. Renders the prompt', () => {
     const wrapper = shallow(<GameInput wordInfo={{word: 'ball', type: 'NN', index: 1}}/>);
 
     expect(wrapper.find('h4').text()).toEqual('singular noun')
+  });
+
+  it('2. Renders with an input', () => {
+    const wrapper = shallow(<GameInput wordInfo={{word: 'ball', type: 'NN', index: 1}}/>);
+
+    expect(wrapper.find('input').length).toEqual(1)
+  });
+
+  it('3. Input should update with value from state', () => {
+    const wrapper = mount(<GameInput wordInfo={{word: 'ball', type: 'NN', index: 1}}/>);
+
+    expect(wrapper.state('selected')).toEqual(false)
+    wrapper.simulate('focus');
+    expect(wrapper.state('selected')).toEqual(true)
+  });
+
+  it('4. onblur input should be sent to App', () => {
+    const mockFunc = jest.fn()
+    const wrapper = mount(<GameInput wordInfo={{word: 'ball', type: 'NN', index: 1}} onBlur={mockFunc}/>);
+
+    wrapper.simulate('blur');
+    expect(mockFunc.mock.calls.length).toBe(1);
   });
 });
